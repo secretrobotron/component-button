de62333256d6ed72132c8af493122af9378417ad
David Ascher <david.ascher@gmail.com>
2013-11-14 22:43:25 -0800

    avoid double-running with remote polymer, and allow cors request to fetch dependent components

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    request = require('request'),
+    http = require('http');
+
 var argv = require('optimist').argv;
+
+var app = express();
 
 // in offline mode, serve the test assets from the appmaker/serve_test_assets.js server.
 // http://localhost:4321
@@ -8,19 +12,19 @@ var argv = require('optimist').argv;
 var TEST_URL = "http://localhost:4321/";
 var areOffline = false;
 if (argv.o) {
-	console.log("The internet isn't around, so we'll get testing assets from:\n  ", TEST_URL);
-	areOffline = true;
+  console.log("The internet isn't around, so we'll get testing assets from:\n  ", TEST_URL);
+  areOffline = true;
 }
 
 if (areOffline) {
-	app.use(function (req, res, next) {
-		var write = res.write;
-	    res.write = function(chunk, encoding){
-	    	var s = String(chunk).split('https://appmaker.mozillalabs.com/test_assets/').join(TEST_URL);
-	      return write.call(res, s, encoding);
-	    };
-		next();
-	});
+  app.use(function (req, res, next) {
+    var write = res.write;
+      res.write = function(chunk, encoding){
+        var s = String(chunk).split('https://appmaker.mozillalabs.com/test_assets/').join(TEST_URL);
+        return write.call(res, s, encoding);
+      };
+    next();
+  });
 }
 
 var allowCrossDomain = function(req, res, next) {
@@ -30,7 +34,29 @@ var allowCrossDomain = function(req, res, next) {
 
     next();
 }
-
 app.use(allowCrossDomain);
+
+app.get('/component/:id', function(req, res) {
+  var componentName = req.params['id'];
+  var url = "http://mozilla-appmaker.github.io/" + componentName + "/component.html";
+  if (url) {
+    try {
+      request.get(url).on('error',
+        function(err) { console.log('error doing cors request for ', url);})
+      .pipe(res)
+      .on('error',
+        function(err) { console.log('error doing cors request for ', url);});
+    } catch (e) {
+      console.log("got exception doing the pipe", e);
+      res.json({message: 'No valid url.'}, 500);
+      return;
+    }
+  }
+  else {
+    res.json({error: 'No valid url.'}, 500);
+  }
+});
+
 app.use(express.static(__dirname));
+
 app.listen(1234);

e8e0306c0dfb1fb97b7fe74a611075586eaccf55
David Ascher <david.ascher@gmail.com>
2013-11-25 13:27:30 -0800

    cleanup

--- test/js/button.js
+++ test/js/button.js
@@ -1,3 +1,5 @@
+// could do regular JS tests:
+
 // suite('button', function() {
 //   setup(function() {
 //     work = document.createElement('div');
@@ -13,13 +15,10 @@
 //   var assert = chai.assert;
 //   test('button test', function() {
 //     assert.equal(1, 1);
-//     var app = document.querySelector('ceci-app');
-//     // console.log("GOT THE APP", app);
-//     console.log(app.addCard);
-//     // console.log(document.head);
-//     // console.log(document.body);
 //   });
 // });
+
+// or load an HTML test case:
 
 htmlSuite('element registration', function() {
   htmlTest('test/html/buttonSpec.html');
